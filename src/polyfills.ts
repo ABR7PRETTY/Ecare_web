9228584e099e341e384affa35f22c35d73796e78
ABR7PRETTY <abr7pretty@gmail.com>
2025-06-03 21:03:20 +0000

    vercel deployment

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,6 +1,5 @@
 /***************************************************************************************************
  * Zone JS is required by Angular itself.
  */
-import 'zone.js';  // Included with Angular CLI.
 
 (window as any).global = window; // 👈 Ajoute cette ligne ici
